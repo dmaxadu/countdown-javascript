6fc5baa6dbd90dee711e48921d57e1312d501906
dmaxadu <dmaxadu@ufrj.br>
2023-01-22 22:45:27 -0300

    almost done ⌛

---
+++ scripts.js
@@ -0,0 +1,5 @@
+let days = document.querySelector(".days");
+let hours = document.querySelector(".hours");
+let minutes = document.querySelector(".minutes");
+let seconds = document.querySelector(".seconds");
+let concertDay = new Date('March 04, 2023 21:30 GMT-03:00');
